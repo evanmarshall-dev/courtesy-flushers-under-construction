ebf52f50c483ec09d4eb745f0567983dcd3f18e6
evanmarshall-dev <me@evanmarshall.dev>
2022-08-16 02:05:31 -0300

    Made minor tweaks to components in order to deploy to vercel.

--- components/Footer.js
+++ components/Footer.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      <footer className="footer">
-        <div className="pb-8 mb-2 border-t-2 border-brand-primary dark:border-brand-light">
-          <div>
-            <p>This is a placeholder for footer and copyright</p>
-          </div>
+    <footer className="footer">
+      <div className="pb-8 mb-2 border-t-2 border-brand-primary dark:border-brand-light">
+        <div>
+          <p>This is a placeholder for footer and copyright</p>
         </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 };
 
