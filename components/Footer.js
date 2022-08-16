1bc5ab05e27accb1d55bb58ef346819ae7e95a37
evanmarshall-dev <me@evanmarshall.dev>
2022-08-16 02:29:50 -0300

    ESLint error called for removal of return (). However page did not load after vercel deployment. I have added returns back in and page loads.

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,13 @@
 const Footer = () => {
-  <footer>
-    <div className="pb-8 mb-2 border-t-2 border-brand-primary dark:border-brand-light">
-      <div>
-        <p>This is a placeholder for footer and copyright</p>
+  return (
+    <footer>
+      <div className="pb-8 mb-2 border-t-2 border-brand-primary dark:border-brand-light">
+        <div>
+          <p>This is a placeholder for footer and copyright</p>
+        </div>
       </div>
-    </div>
-  </footer>;
+    </footer>
+  );
 };
 
 export default Footer;
